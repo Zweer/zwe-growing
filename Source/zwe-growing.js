2c0f045cc21e2fcd2e88d07cc4af7dbc97c17121
Niccolo' Olivieri <flicofloc@gmail.com>
2011-09-15 15:00:32 +0200

    Ok, the work is completed...

--- Source/zwe-growing.js
+++ Source/zwe-growing.js
@@ -2,46 +2,145 @@ var ZweGrowing = new Class({
     Implements: [Options, Events],
 
     options: {
-
-    }
-});
-
-ZweGrowing.Input = new Class();
-
-ZweGrowing.TextArea = new Class({
-    Implements: [Options, Events],
-
-    options: {
         min: 20,
+        extraSpace: 10,
 
         duration: 200,
-        transition: Fx.Transitions.Sine.easeOut
+        transition: Fx.Transitions.Sine.easeOut/*,
+
+        onResize: function(){},
+        onResized: function(){}*/
     },
 
     element: null,
+    clone: null,
     animation: null,
+
+    originalSize: 0,
+    scroll: 0,
+    lastScroll: 0,
+
+    property: null,
 
     initialize: function(element, options)
     {
+        if(!this.property || !['height', 'width'].contains(this.property))
+            return;
+
         this.setOptions(options);
-        this.element = document.id(element).setStyle('overflow', 'hidden');
-        this.animation = new Fx.Tween(this.element, {
-            property: 'height',
-            duration: this.options.duration,
-            transition: this.options.transition
+        this.element = document.id(element).setStyles({
+            resize: 'none',
+            overflowY: 'hidden'
         });
 
-        this.element.addEvent('keyup', this._resize.bind(this));
+        this.originalSize = this.element.getStyle(this.property).toInt();
+
+        this._addClone();
+        this._addAnimation();
+        this._addEvents();
         this._resize();
+    },
+
+    _addClone: function()
+    {
+        this.clone.inject(document.body);
+        ['font-size', 'font-family', 'padding-left', 'padding-top', 'padding-bottom',
+		 'padding-right', 'border-left', 'border-right', 'border-top', 'border-bottom',
+		 'word-spacing', 'letter-spacing', 'text-indent', 'text-transform', 'text-decoration',
+         'line-height'].each(function(property){
+            this.clone.setStyle(property, this.element.getStyle(property));
+        }, this);
+    },
+
+    _addAnimation: function()
+    {
+        this.animation = new Fx.Tween(this.element, {
+            property: this.property,
+            duration: this.options.duration,
+            transition: this.options.transition
+        })
+    },
+
+    _addEvents: function()
+    {
+        this.element.addEvents({
+            keyup: this._resize.bind(this),
+            keydown: this._resize.bind(this),
+            change: this._resize.bind(this)
+        });
     },
 
     _resize: function()
     {
-        var size = this.element.getStyle('height').toInt(),
-            scroll = this.element.getScrollSize();
+        this._calculate();
 
-        if(scroll.y > this.options.min && size != scroll.y)
-            this.animation.start(size, scroll.y);
+        if(this.lastScroll === this.scroll)
+            return;
+
+        this.lastScroll = this.scroll;
+
+        this.fireEvent('resize');
+
+        this.animation.start(this.element.getStyle(this.property), this.scroll).chain(this.fireEvent.bind(this, 'resized'));
+    },
+
+    _calculate: function()
+    {
+        // To be overridden
+    }
+});
+
+ZweGrowing.Input = new Class({
+    Extends: ZweGrowing,
+
+    options: {
+        duration: 0
+    },
+
+    property: 'width',
+
+    _addClone: function()
+    {
+        this.clone = new Element('span', {
+            styles: {
+                'float': 'left',
+                display: 'inline-block',
+                position: 'absolute',
+                left: -9999
+            }
+        });
+        this.parent();
+    },
+
+    _calculate: function()
+    {
+        this.clone.set('text', this.element.get('value'));
+        this.scroll = Math.max(this.originalSize, this.clone.getStyle(this.property).toInt()) + this.options.extraSpace;
+    }
+});
+
+ZweGrowing.TextArea = new Class({
+    Extends: ZweGrowing,
+
+    property: 'height',
+
+    _addClone: function()
+    {
+        this.clone = this.element.clone().set('name', '').setStyles({
+            position: 'absolute',
+            top: 0,
+            left: -9999,
+            height: this.element.getStyle('height'),
+            width: this.element.getStyle('width')
+        });
+        this.parent();
+    },
+
+    _calculate: function()
+    {
+        this.clone.setStyle('height', 0);
+        this.clone.set('value', this.element.get('value'));
+        this.scroll = Math.max(this.originalSize, this.clone.getScrollSize().y) + this.options.extraSpace;
     }
 });
 
